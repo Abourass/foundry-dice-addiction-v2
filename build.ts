f342e848e7970d9d5bde692b996cfa3570015a14
Abourass <Abourassa@AssetVal.com>
2023-01-26 11:49:03 -0700

    :sparkles: Building so we can auto convert to webp

--- build.ts
+++ build.ts
@@ -1,7 +1,59 @@
 import fs from 'fs';
 import archiver from 'archiver';
+import sharp from 'sharp';
 
-function buildPackage(){
+async function buildPackage(){
+  // We start by copying our bump maps into our src/textures/bump directory, after converting them to webp
+  const bumpMaps = fs.readdirSync('./assets/bump');
+
+  await Promise.all([
+    bumpMaps.map(async(bumpMap) => {
+      const bumpMapName = bumpMap.split('.')[0];
+
+      if (!fs.existsSync(`./src/textures/bump/${bumpMapName}.webp`)){
+        const file = fs.readFileSync(`./assets/bump/${bumpMap}`);
+
+        const convertedFile = await sharp(file).webp({ lossless: true }).toBuffer();
+
+        fs.writeFileSync(`./src/textures/bump/${bumpMapName}.webp`, convertedFile);
+      }
+    })
+  ]);
+
+  // Then we will copy our dice faces into our src/faces directory, after converting them to webp
+  const diceFaces = fs.readdirSync('./assets/faces');
+
+  await Promise.all([
+    diceFaces.map(async(diceFace) => {
+      const diceFaceName = diceFace.split('.')[0];
+
+      if (!fs.existsSync(`./src/faces/${diceFaceName}.webp`)){
+        const file = fs.readFileSync(`./assets/faces/${diceFace}`);
+
+        const convertedFile = await sharp(file).webp({ lossless: true }).toBuffer();
+
+        fs.writeFileSync(`./src/faces/${diceFaceName}.webp`, convertedFile);
+      }
+    })
+  ]);
+
+  // Then we will copy our dice textures into our src/textures directory, after converting them to webp
+  const diceTextures = fs.readdirSync('./assets/textures');
+
+  await Promise.all([
+    diceTextures.map(async(diceTexture) => {
+      const diceTextureName = diceTexture.split('.')[0];
+
+      if (!fs.existsSync(`./src/textures/${diceTextureName}.webp`)){
+        const file = fs.readFileSync(`./assets/textures/${diceTexture}`);
+
+        const convertedFile = await sharp(file).webp({ lossless: true }).toBuffer();
+
+        fs.writeFileSync(`./src/textures/${diceTextureName}.webp`, convertedFile);
+      }
+    })
+  ]);
+
   // We will start by getting the package.json
   const packageDeclaration = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
@@ -52,4 +104,6 @@ function buildPackage(){
   archive.finalize();
 }
 
-buildPackage();
+(async() => {
+  await buildPackage();
+})()
